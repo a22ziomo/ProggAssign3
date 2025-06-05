7ad01415aa4116784273fd8cad1c61bc0d67a332
a22ziomo <a22ziomo@student.his.se>
2025-06-05 22:13:08 +0200

    Light at the end of my Face
    WAH

--- Assets/Scripts/Findpather.cs
+++ Assets/Scripts/Findpather.cs
@@ -3,19 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using Grid;
+using Unity.VisualScripting;
 using Vectors;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
 [ExecuteAlways]
 [RequireComponent(typeof(VectorRenderer))]
 
 public class Findpather : MonoBehaviour
 {
-    [SerializeField] private GameObject gamer;
+    [SerializeField] private int ExtraMoveCost;
     [SerializeField] private Vector2 startPosition;
     [SerializeField] public int maximumSteps;
     private Vector2 size;
-    //private Dictionary<Vector2, Tile> Tiles;
+    private Dictionary<Vector2, Piece> Tiles;
     [SerializeField] private List<Vector2> goals;
     [SerializeField] private List<Vector2> portalen;
     
@@ -23,48 +25,60 @@ public class Findpather : MonoBehaviour
 
     private void OnEnable()
     {
-        vectors.GetComponent<VectorRenderer>();
+        vectors = GetComponent<VectorRenderer>();
     }
-    /*
+    
     // Update is called once per frame
     void Update()
     {
-        Tiles = new Dictionary<Vector2, Tile>();
+        Tiles = new Dictionary<Vector2, Piece>();
         size = GetComponent<Grid.Grid>().Size;
-        Vector3 temporaryStart = new Vector3(startPosition.x, .25f, startPosition.y);
-        
-        if (gamer.transform.position != temporaryStart)
-        {
-            gamer.transform.position = temporaryStart;      
-        }
 
         goals = new List<Vector2>();
         portalen = new List<Vector2>();
         foreach (Transform offspring in this.transform)
         {
-            if (offspring == gamer.transform)
-            {
-                continue;
-            }
-
             Vector2 pos = new Vector2(offspring.position.x, offspring.position.z);
-            Tile tile = offspring.GetComponent<Tile>();
+            Piece tile = offspring.GetComponent<Piece>();
             Tiles.Add(pos,tile);
 
-            if (tile.state.GetProperty(TileTypes.Goal))
+            if (tile.state.GetProperty(TileProperty.Goal))
             {
                 goals.Add(pos);
             }
 
-            if (tile.state.GetProperty(TileTypes.Portal))
+            if (tile.state.GetProperty(TileProperty.Portal))
             {
                 portalen.Add(pos);
             }
         }
 
+        foreach (Piece tile in Tiles.Values)
+        {
+            tile.G = -1;
+        }
+        
+        FindG(startPosition);
+        
+
         foreach (Vector2 goal in goals)
         {
             List<Vector2> path = FindPath(startPosition, goal);
+            if (path != null)
+            {
+                using (vectors.Begin())
+                {
+                    for (int i = 0; i < path.Count; i++)
+                    {
+                        if (!((int)path[i].x == (int)goal.x && (int)path[i].y == (int)goal.y))
+                        {
+                            Vector3 from = new Vector3(path[i].x, 0.1f, path[i].y);
+                            Vector3 to = new Vector3(path[i + 1].x, 0.1f, path[i + 1].y);
+                            vectors.Draw(from,to,Color.grey);
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -72,7 +86,7 @@ public class Findpather : MonoBehaviour
     {
         List<Vector2> openList = new List<Vector2>();
         List<Vector2> closedList = new List<Vector2>();
-        if (!Tiles[start].state.GetProperty(TilesTypes.Blocked))
+        if (!Tiles[start].state.GetProperty(TileProperty.Blocked))
         {
             Tiles[start].G = 0;
             openList.Add(start);
@@ -87,11 +101,11 @@ public class Findpather : MonoBehaviour
             {
                 foreach (Vector2 neighbour in GetNeighbours(current))
                 {
-                    Tile tile = Tiles[neighbour];
+                    Piece tile = Tiles[neighbour];
                     int tempG = Tiles[current].G + 1;
-                    if (tile.state.GetProperty(TilesTypes.Obstacle))
+                    if (tile.state.GetProperty(TileProperty.Hinder))
                     {
-                        tempG += tile.ExtraMoveCost;
+                        tempG += ExtraMoveCost;
                     }
 
                     if (closedList.Contains(neighbour))
@@ -122,7 +136,7 @@ public class Findpather : MonoBehaviour
     {
         List<Vector2> openList = new List<Vector2>();
         List<Vector2> closedList = new List<Vector2>();
-        if (!Tiles[start].state.GetProperty(TileTypes.Blocked))
+        if (!Tiles[start].state.GetProperty(TileProperty.Blocked))
         {
             Tiles[start].G = 0;
             openList.Add(start);
@@ -134,11 +148,11 @@ public class Findpather : MonoBehaviour
 
             if (current_ == end)
             {
-                return ReconstructPath(current);
+                return ReconstructPath(current_);
             }
             
             openList.Remove(current_);
-            closedList.Remove(current_);
+            closedList.Add(current_);
 
             foreach (Vector2 neighbour in GetNeighbours(current_))
             {
@@ -147,11 +161,11 @@ public class Findpather : MonoBehaviour
                     continue;
                 }
 
-                Tile tile = Tiles[neighbour];
+                Piece tile = Tiles[neighbour];
                 int tempG = Tiles[current_].G + 1;
-                if (tile.state.GetProperty(TileTypes.Obstacle))
+                if (tile.state.GetProperty(TileProperty.Hinder))
                 {
-                    tempG += tile.ExtraMoveCost;
+                    tempG += ExtraMoveCost;
                 }
 
                 if (!openList.Contains(neighbour))
@@ -164,7 +178,101 @@ public class Findpather : MonoBehaviour
                     tile.G = tempG;
                 }
 
+                if (tempG <= tile.G)
+                {
+                    tile.Parent = current_;
+                }
+
+                tile.H = calcH(current_, end);
+
             }
         }
-    }*/
+
+        return null;
+    }
+
+    private List<Vector2> GetNeighbours(Vector2 origin)
+    {
+        List<Vector2> neighbours = new List<Vector2>();
+        Piece originTile = Tiles[origin];
+        Vector2[] directions = new Vector2[]
+            { new Vector2(0, 1), new Vector2(1, 0), new Vector2(-1, 0), new Vector2(0, -1) };
+
+        foreach (Vector2 direction in directions)
+        {
+            float x = origin.x + direction.x;
+            float y = origin.y + direction.y;
+
+            if (x >= 0 && x < size.x && y >= 0 && y < size.y && !Tiles[new Vector2(x,y)].state.GetProperty(TileProperty.Blocked))
+            {
+                neighbours.Add(new Vector2(x,y));
+            }
+            
+        }
+
+        if (originTile.state.GetProperty(TileProperty.Portal))
+        {
+            neighbours.Add(originTile.PortalExitCoord);
+        }
+
+        return neighbours;
+    }
+
+    private int calcH(Vector2 start, Vector2 end, bool checkPortals = true)
+    {
+        List<int> hList = new List<int>();
+        hList.Add((int)(Mathf.Abs(start.x - end.x) + Mathf.Abs(start.y - end.y)));
+
+        if (checkPortals)
+        {
+            foreach (Vector2 portal in portalen)
+            {
+                hList.Add((int)(calcH(start, end, false) + 1 + calcH(Tiles[portal].PortalExitCoord, end, false)));
+            }
+        }
+
+        int lowest = hList[0];
+        foreach (int h in hList)
+        {
+            if (h < lowest)
+            {
+                lowest = h;
+            }
+        }
+
+        return lowest;
+    }
+
+    private List<Vector2> ReconstructPath(Vector2 goal)
+    {
+        List<Vector2> path = new List<Vector2>();
+        while (!((int)goal.x == (int)startPosition.x && (int)goal.y == (int)startPosition.y))
+        {
+            path.Add(goal);
+            goal = Tiles[goal].Parent;
+        }
+        path.Add(startPosition);
+        path.Reverse();
+        return path;
+    }
+
+    private Vector2 GetLowestF(List<Vector2> list)
+    {
+        Vector2 lowest = list[0];
+        foreach (Vector2 tile in list)
+        {
+            if (Tiles[tile].F < Tiles[lowest].F)
+            {
+                lowest = tile;
+            }
+        }
+
+        return lowest;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawCube(new Vector3(startPosition.x, 1, startPosition.y),new Vector3(0.5f, 0.5f, 0.5f) );
+    }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -8,34 +8,52 @@ using UnityEngine;
 [RequireComponent(typeof(GridTile))]
 public class Piece : MonoBehaviour
 {
+    public GridTile state;
+    public int G;
+    public int H;
+    public int F => G + H;
+    public Vector2 Parent;
+    public Vector2Int PortalExitCoord;
+    
     private void OnDrawGizmos()
     {
         var tile = GetComponent<GridTile>();
-        if (tile.GetProperty(GridTileProperty.Solid))
+        if (tile.GetProperty(TileProperty.Blocked))
         {
             Gizmos.color = Color.red;
+        }
+        else if (state.GetProperty(TileProperty.Hinder))
+        {
+            Gizmos.color = Color.yellow;
         }
         else
         {
             Gizmos.color = Color.green;
         }
-
         Gizmos.DrawCube(transform.position, new Vector3(1, 0.1f, 1));
-        if (tile.GetNeighbourProperty(0, GridTileProperty.Solid))
+
+
+        if (tile.GetProperty(TileProperty.Portal))
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position+Vector3.up, 0.5f);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(new Vector3(PortalExitCoord.x, 1, PortalExitCoord.y), 0.5f);
+            
+        }
+
+        if (tile.GetProperty(TileProperty.Goal))
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transform.position+new Vector3(0,0.5f,0), 0.25f);
+            
+            
+        }
+        
+        if (tile.GetNeighbourProperty(0, TileProperty.Blocked))
         {
             Gizmos.color = Color.yellow;
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
